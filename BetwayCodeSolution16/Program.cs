64a98fad54e88055b68c2d5b621d26116da584a3
Nombongo Gxekiwe <s223326534@mandela.ac.za>
2025-02-07 23:36:09 +0200

    Add project files.

---
+++ BetwayCodeSolution16/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BetwayCodeSolution16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "Za^B8g@E2jH*kWl!MoPqXr7YvT1c$Fs3Ud9IwZ&yX0pLkV6sHqN^tB4rA+oZ%gFj";
+            string output = ReformatAlternatingCase(input);
+            Console.WriteLine(output);
+            Console.ReadLine();
+        }
+        static string ReformatAlternatingCase(string s)
+        {
+            char[] result = new char[s.Length];
+            bool upper = true; 
+            int alphaIndex = 0;
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (char.IsLetter(s[i]))
+                {
+                    result[i] = upper ? char.ToUpper(s[i]) : char.ToLower(s[i]);
+                    upper = !upper; 
+                }
+                else
+                {
+                    result[i] = s[i]; 
+                }
+            }
+            return new string(result);
+        }
+    }
+}
